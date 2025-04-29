841903318825dc38b7a8c2683a81d85876ea11c7
000begana000 <begana.contact@gmail.com>
2025-04-29 19:36:22 +0200

    feat: create Projects component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header/Header";
+import Projects from "./components/Projects/Projects";
 
 export default function App() {
   return (
     <>
       <Header />
+      <main>
+        <Projects />
+      </main>
     </>
   );
 }
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,10 @@
+export default function Projects() {
+  return (
+    <section>
+      <h1>To Do</h1>
+      <ul>
+        <li>Project 1</li>
+      </ul>
+    </section>
+  );
+}
