5425fd61f652ade045e219ff5d67e4ed79eace9b
000begana000 <begana.contact@gmail.com>
2025-05-03 12:09:43 +0200

    fix: adjust label classname

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -3,7 +3,7 @@ import classes from "./Input.module.css";
 export default function Input({ label, textarea, ...props }) {
   return (
     <p>
-      <label>{label}</label>
+      <label className={classes.label}>{label}</label>
       {textarea ? (
         <textarea {...props} className={classes.text} />
       ) : (
