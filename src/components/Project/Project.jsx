182e938eccc48dac4854dff1544e0efa42c537a3
000begana000 <begana.contact@gmail.com>
2025-05-15 13:01:30 +0200

    fix: collapse icon style

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
 import classes from "./Project.module.css";
 
 import Button from "../UI/Button";
@@ -6,6 +9,9 @@ import Button from "../UI/Button";
 // change input field to Input component
 export default function Project({ project }) {
   const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const collapseUpIcon = <FontAwesomeIcon icon={faChevronUp} />;
+  const collapseDownIcon = <FontAwesomeIcon icon={faChevronDown} />;
 
   // toggle collapse when user clicks collapse button
   function toggleCollapse() {
@@ -16,23 +22,28 @@ export default function Project({ project }) {
     <>
       <li className={classes.project} key={project.id}>
         <div className={classes.projectCheckbox}>
-          <input
-            type="checkbox"
-            id="scales"
-            name="scales"
-            checked={project.isDone ? ture : false}
-            className={`${classes.checkbox} ${classes.test}`}
-          />
-          <label htmlFor="scales" className={classes.projectTitle}>
-            {project.title}
-          </label>
-          <button
-            className={classes.collapseButton}
-            onClick={toggleCollapse}
-            aria-label="Toggle project details"
-          >
-            {isCollapsed ? "▶" : "▼"} {/* Arrow icon changes based on state */}
-          </button>
+          <div>
+            <input
+              type="checkbox"
+              id="scales"
+              name="scales"
+              checked={project.isDone ? ture : false}
+              className={`${classes.checkbox} ${classes.test}`}
+            />
+            <label htmlFor="scales" className={classes.projectTitle}>
+              {project.title}
+            </label>
+          </div>
+          <div>
+            <button
+              className={classes.collapseButton}
+              onClick={toggleCollapse}
+              aria-label="Toggle project details"
+            >
+              {isCollapsed ? collapseDownIcon : collapseUpIcon}{" "}
+              {/* Arrow icon changes based on state */}
+            </button>
+          </div>
         </div>
 
         {!isCollapsed && (
