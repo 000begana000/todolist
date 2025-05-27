86c905a3db6ff5ea13976258f36c77e54b29a72b
000begana000 <begana.contact@gmail.com>
2025-05-27 13:21:54 +0200

    feat: add Projects and NewProjects components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Header from "./components/Header/Header";
+import Projects from "./components/Projects/Projects";
+import NewProject from "./components/NewProject/NewProject";
 
 export default function App() {
   return (
     <>
       <Header />
+      <main>
+        <Projects />
+        <NewProject />
+      </main>
     </>
   );
 }
