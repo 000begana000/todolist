def512611687b431aeae29913a38e6fe87ea5d00
000begana000 <begana.contact@gmail.com>
2025-05-28 11:34:56 +0200

    feat: test ProjectContext in Projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,16 @@
+import { useContext } from "react";
+import ProjectContext from "../../store/ProjectContext";
+
 import styles from "./Projects.module.css";
 
 export default function Projects() {
+  const projectContext = useContext(ProjectContext);
+  const { projects } = projectContext;
+
   return (
     <section className={styles.projects}>
       <h1 className={styles.heading}>to do</h1>
+      {projects.length === 0 && <p>create new project</p>}
     </section>
   );
 }
--- src/store/ProjectContext.jsx
+++ src/store/ProjectContext.jsx
@@ -2,17 +2,35 @@ import { createContext, useReducer } from "react";
 
 const ProjectContext = createContext({
   projects: [],
+  addProject: project => {},
 });
 
-function projectReducer() {}
+function projectReducer(state, action) {
+  if (action.type === "ADD_PROJECT") {
+    const newProject = action.project;
+    const updatedProjects = [newProject, ...state.projects];
+
+    return { ...state, projects: updatedProjects };
+  }
+
+  return state;
+}
 
 export function ProjectContextProvider({ children }) {
   const [projectsState, dispatchProjectAction] = useReducer(projectReducer, {
     projects: [],
   });
 
+  function addProject(project) {
+    dispatchProjectAction({
+      type: "ADD_PROJECT",
+      project,
+    });
+  }
+
   const cartCtxValue = {
     projects: projectsState.project,
+    addProject,
   };
 
   return (
