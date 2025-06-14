bb7b412e6488d4a4e43a2a69eeb21d16a051d67f
000begana000 <begana.contact@gmail.com>
2025-06-14 15:23:02 +0200

    import TaskContext and map the tasks

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,3 +1,9 @@
+//// hook
+import { useContext } from "react";
+
+//// context
+import TaskContext from "../../store/TaskContext";
+
 //// component
 import NewTask from "../NewTask/NewTask";
 
@@ -5,9 +11,18 @@ import NewTask from "../NewTask/NewTask";
 import styles from "./Tasks.module.css";
 
 export default function Tasks() {
+  const taskCtx = useContext(TaskContext);
+  const { tasks } = taskCtx;
+
   return (
     <div className={styles.container}>
       <NewTask />
+      {tasks.map(task => (
+        <>
+          <input type="checkbox" />
+          <label key={task.id}>{task.description}</label>
+        </>
+      ))}
     </div>
   );
 }
